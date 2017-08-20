632ebfeb6b0d5e3efd366e7a9800addbab0561a9
laitingyou <1003774479@qq.com>
2017-08-20 13:38:12 +0800

    #finish All WEB

---
+++ Template/Admin/source/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+import './App.css';
+import { Modal, Button } from 'antd';
+class App extends Component {
+    state = { visible: false }
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+    handleOk = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    }
+    handleCancel = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    }
+    render() {
+        return (
+            <div>
+              <Button type="primary" onClick={this.showModal}>Open</Button>
+              <Modal
+                  title="Basic Modal"
+                  visible={this.state.visible}
+                  onOk={this.handleOk}
+                  onCancel={this.handleCancel}
+              >
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+              </Modal>
+            </div>
+        );
+    }
+}
+
+export default App;
