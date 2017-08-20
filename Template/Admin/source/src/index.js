42732bc237b377aac8cbd26067e7e2d142d31953
laitingyou <1003774479@qq.com>
2017-08-20 16:55:10 +0800

    #React Router

--- Template/Admin/source/src/index.js
+++ Template/Admin/source/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Router, Route, IndexRoute, Redirect,Link, hashHistory } from 'react-router';
+ReactDOM.render(
+    <Router history={hashHistory}>
+            <Route path="/" component={App} />
+    </Router>
+    , document.getElementById('root'));
 registerServiceWorker();
